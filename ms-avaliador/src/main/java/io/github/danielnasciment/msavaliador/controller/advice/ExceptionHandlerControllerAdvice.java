6f2cf52c345edf66fc7b61a15820acf1635d364f
Daniel-Nasciment <daniel.fecaf@outlook.com>
2023-03-04 22:17:27 -0300

    Feat: Implementando handler

---
+++ ms-avaliador/src/main/java/io/github/danielnasciment/msavaliador/controller/advice/ExceptionHandlerControllerAdvice.java
@@ -0,0 +1,47 @@
+package io.github.danielnasciment.msavaliador.controller.advice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import io.github.danielnasciment.msavaliador.exeptions.CpfNotFoundException;
+
+@ControllerAdvice
+public class ExceptionHandlerControllerAdvice {
+
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public ResponseEntity<?> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+		
+		List<String> fieldsError = new ArrayList<>();
+		
+		ex.getFieldErrors().forEach(f -> fieldsError.add("PARAMETRO: [" + f.getField() + "] Mensagem: [" + f.getDefaultMessage() + "]"));
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDetails(
+				"Por favor, verifique se todos os campos foram preenchidos corretamente!", 
+				HttpStatus.BAD_REQUEST.value(), 
+				fieldsError,
+				new Date().getTime()));
+	}
+	
+	@ExceptionHandler(CpfNotFoundException.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public ResponseEntity<?> handlerClienteNotFoundException(CpfNotFoundException ex){
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDetails(
+				"Cliente nao encontrado!", 
+				HttpStatus.NOT_FOUND.value(), 
+				Arrays.asList(ex.getMessage()),
+				new Date().getTime()));
+	}
+	
+}
